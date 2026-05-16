435ee45d903b82164a15e1a489e5d30d85474656
vonvit <sandrineviturn@gmail.com>
2026-05-16 20:50:46 -0300

    Exception handling - update

--- src/main/java/com/vonvit/course/services/UserService.java
+++ src/main/java/com/vonvit/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.vonvit.course.entities.User;
 import com.vonvit.course.repositories.UserRepository;
 import com.vonvit.course.services.exceptions.DatabaseException;
 import com.vonvit.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
@@ -45,9 +46,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
