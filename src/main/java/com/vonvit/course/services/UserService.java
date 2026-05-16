248f7f7fcfe2f733de7313f1d490e6f89fb9e3db
vonvit <sandrineviturn@gmail.com>
2026-05-16 19:24:56 -0300

    Exception handling - findById

--- src/main/java/com/vonvit/course/services/UserService.java
+++ src/main/java/com/vonvit/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.vonvit.course.services;
 
 import com.vonvit.course.entities.User;
 import com.vonvit.course.repositories.UserRepository;
+import com.vonvit.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/vonvit/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vonvit.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. id " + id);
+    }
+}
